8fa0d07bf37ac228bc2c96bf661dd9931a4f3d4c
CamiloMunozAL <camilo.munoz2@utp.edu.co>
2026-03-09 19:37:45 -0500

    add 600 in linux scrupt

--- case1_matrix_mul/hilos/run_hilos_linux.sh
+++ case1_matrix_mul/hilos/run_hilos_linux.sh
@@ -23,7 +23,7 @@ echo "Ejecutando benchmarks..."
 # 10 rondas para cada tamaño y número de hilos
 for round in {1..10}; do
     echo "  Ronda $round/10..."
-    for size in 400 800 1000 2000 4000 6000; do
+    for size in 400 600 800 1000 2000 4000 5500 6000 8000; do
         for threads in 2 4 8 16; do
             "$BIN" "$size" "$threads" 1 123456789 >> "$OUT"
         done
